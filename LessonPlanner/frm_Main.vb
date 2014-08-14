1281a1b64880b4c587e42b512f3e165eccb19b9d
drew_desktop <drew.hardwick@gmail.com>
2014-08-14 08:14:40 -0500

    Base Layout

--- LessonPlanner/frm_Main.vb
+++ LessonPlanner/frm_Main.vb
@@ -1,3 +1,31 @@
 ﻿Public Class frm_main
 
+#Region "Variable Def"
+
+#End Region
+
+#Region "Menu Click Handlers"
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub ExportToPDFToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExportToPDFToolStripMenuItem.Click
+
+    End Sub
+
+#End Region
+
 End Class
